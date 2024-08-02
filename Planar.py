36542c47fee3adee8d405bc39c6d95b683ef9c40
Perucia <peruciaaa@gmail.com>
2024-08-03 04:52:16 +0800

    Update Planar.py

---
+++ Planar.py
@@ -0,0 +1,11 @@
+from equipment import Equipment
+
+class Planar(Equipment):
+    name = "Planar"
+    
+    def __init__(self):
+        pass
+        
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
