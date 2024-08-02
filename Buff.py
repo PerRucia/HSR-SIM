d8f5a64a7ff693bc2e748c925226a8176d46d9f0
Perucia <peruciaaa@gmail.com>
2024-08-02 20:39:29 +0800

    Update Buff.py

--- Buff.py
+++ Buff.py
@@ -25,19 +25,54 @@ atkTypes = ["BASIC", "SKILL", "ULT", "FUA", "ALL"]
 '''
 
 class Buff:
-    def __init__(self, name=str, buffType=str, val=float, target=list, atkType=list, turns=int):
+    def __init__(self, name=str, buffType=str, val=float, target=list, atkType=list, turns=int, stackLimit=int):
         self.name = name
         self.buffType = buffType
         self.val = val
         self.target = target
         self.atkType = atkType
         self.turns = turns
+        self.stackLimit = stackLimit
+        self.stacks = 1
+        
+    def reduceTurns(self) -> None:
+        self.turns = self.turns - 1
+    
+    def refreshTurns(self) -> None:
+        self.turns = self.storedTurns
+        
+    def increaseStacks(self) -> None:
+        if self.stacks == self.stackLimit:
+            return
+        self.stacks = self.stacks + 1
+        self.refreshTurns()
+        
+    def getBuffVal(self) -> float:
+        return self.val * self.stacks
         
 class Debuff:
-    def __init__(self, name=str, debuffType=str, val=float, target=list, atkType=list, turns=int):
+    def __init__(self, name=str, debuffType=str, val=float, target=list, atkType=list, turns=int, stackLimit=int):
         self.name = name
         self.debuffType = debuffType
         self.val = val
         self.target = target
         self.atkType = atkType
-        self.turns = turns
+        self.storedTurns = turns
+        self.turns = self.storedTurns
+        self.stackLimit = stackLimit
+        self.stacks = 1
+        
+    def reduceTurns(self) -> None:
+        self.turns = self.turns - 1
+    
+    def refreshTurns(self) -> None:
+        self.turns = self.storedTurns
+        
+    def increaseStacks(self) -> None:
+        if self.stacks == self.stackLimit:
+            return
+        self.stacks = self.stacks + 1
+        self.refreshTurns()
+        
+    def getDebuffVal(self) -> float:
+        return self.val * self.stacks
