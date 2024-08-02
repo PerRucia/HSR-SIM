6cb203b4bd85fffa0b3632e61269abff8dd3d41d
Perucia <peruciaaa@gmail.com>
2024-08-03 04:06:52 +0800

    Create equipment.py

---
+++ equipment.py
@@ -0,0 +1,24 @@
+class Equipment:
+    def __init__(self):
+        pass
+    
+    def equip(self): # init function to add base buffs to wearer
+        return "Default equip function called"
+    
+    def useSkl(self):
+        return "Default useSkl function called"
+    
+    def useBsc(self):
+        return "Default useBsc function called"
+    
+    def useUlt(self):
+        return "Default useUlt function called"
+    
+    def useFua(self):
+        return "Default useFua function called"
+    
+    def useHit(self):
+        return "Default useHit function called"
+    
+    def allyTurn(self, turn):
+        return "Default allyTurn function called"
