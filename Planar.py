7cf4dc0f8912977d3cfae56474a04ce967e7089b
Perucia <peruciaaa@gmail.com>
2024-08-07 14:32:14 +0800

    Update Planar.py

--- Planar.py
+++ Planar.py
@@ -3,8 +3,8 @@ from equipment import Equipment
 class Planar(Equipment):
     name = "Planar"
     
-    def __init__(self):
-        pass
+    def __init__(self, wearerRole: str):
+        super().__init__(wearerRole)
         
     def __str__(self) -> str:
         return f"{self.name}"
