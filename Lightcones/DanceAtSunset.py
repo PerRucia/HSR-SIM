af57a62bcacdb9f2c6a67ca5f1ad5c28a4279b65
Perucia <peruciaaa@gmail.com>
2024-08-07 13:27:30 +0800

    Create DanceAtSunset.py

---
+++ Lightcones/DanceAtSunset.py
@@ -0,0 +1,12 @@
+from ..Lightcone import Lightcone
+
+class Sunset(Lightcone):
+    name = "Dance at Sunset"
+    path = "DES"
+    baseHP = 1058.4
+    baseATK = 582.12
+    baseDEF = 463.05
+
+    def __init__(self, level):
+        super(level)
+        
