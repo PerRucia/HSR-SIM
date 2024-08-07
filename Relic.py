057beb664253320ca2ae96d2f6c2367ac9f6fb40
Perucia <peruciaaa@gmail.com>
2024-08-07 14:32:17 +0800

    Update Relic.py

--- Relic.py
+++ Relic.py
@@ -3,7 +3,8 @@ from equipment import Equipment
 class Relic(Equipment):
     name = "Relic"
     
-    def __init__(self, setType: int):
+    def __init__(self, wearerRole: str, setType: int):
+        super().__init__(wearerRole)
         self.setType = setType
         
     def __str__(self) -> str:
