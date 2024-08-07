24fa7f89880eab5216f35fdbcc6756b5130081bf
Perucia <peruciaaa@gmail.com>
2024-08-07 19:26:22 +0800

    Update DanceAtSunset.py

--- Lightcones/DanceAtSunset.py
+++ Lightcones/DanceAtSunset.py
@@ -1,4 +1,5 @@
-from ..Lightcone import Lightcone
+from Lightcone import Lightcone
+from Buff import Buff
 
 class Sunset(Lightcone):
     name = "Dance at Sunset"
@@ -7,6 +8,18 @@ class Sunset(Lightcone):
     baseATK = 582.12
     baseDEF = 463.05
 
-    def __init__(self, level):
-        super(level)
-        
+    def __init__(self, wearerRole, level):
+        super().__init__(wearerRole, level)
+    
+    def equip(self):
+        cdBuff = self.level * 0.06 + 0.3
+        buff_lst = [Buff("SunsetCD", "CD%", cdBuff, self.wearerRole, ["ALL"], 1000, 1)]
+        return buff_lst, []
+    
+    def useUlt(self):
+        buff_lst, debuff_lst = super().useUlt()
+        dmgBuff = self.level * 0.06 + 0.3
+        buff_lst.append(Buff("SunsetDMG", "DMG%", dmgBuff, self.wearerRole, ["ULT", "FUA"], 2, 2))
+        return buff_lst, debuff_lst
+    
+    
