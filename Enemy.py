a74e8447185b38863c6f2170faa076918f683b6f
Perucia <peruciaaa@gmail.com>
2024-08-10 20:13:41 +0800

    Update Enemy.py

--- Enemy.py
+++ Enemy.py
@@ -1,7 +1,27 @@
+# actionOrder = [1,1,2] means single attack, single attack, double attack
+
 class Enemy:
-    level = 95
+    broken = False
     
-    def __init__(self, spd, toughness, actionOrder):
+    def __init__(self, level: int, spd: float, toughness: int, actionOrder: list, weakness: list):
+        self.level = level
         self.spd = spd
         self.toughness = toughness
+        self.gauge = self.toughness
         self.actionOrder = actionOrder
+        self.weakness = weakness
+        
+    def getUniMul(self) -> float:
+        return 1.0 if self.broken else 0.9
+    
+    def redToughness(self, toughness: int) -> bool:
+        self.guage = max(self.gauge - toughness, 0)
+        if self.gauge == 0:
+            self.broken = True
+            return True
+        return False
+    
+    def recover(self):
+        self.gauge = self.toughness
+        self.broken = False
+        
