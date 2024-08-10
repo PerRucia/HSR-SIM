da8fd4b0f8c6cf41a121a958ff4457e7e3d8eb61
Perucia <peruciaaa@gmail.com>
2024-08-10 20:54:52 +0800

    Create Duran.py

---
+++ Planars/Duran.py
@@ -0,0 +1,11 @@
+from Buff import Buff
+from Planar import Planar
+
+class Duran(Planar):
+    def __init__(self, wearerRole: str):
+        super().__init__(wearerRole)
+        
+    def equip(self):
+        buff_lst = [Buff("DuranDMG", "DMG%", 0.25, self.wearerRole, ["FUA"], 1000, 1)]
+        buff_lst.append(Buff("DuranCD", "CD%", 0.25, self.wearerRole, ["ALL"], 1000, 1))
+        return buff_lst, [], [], []
