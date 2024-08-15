bfa4dbbed453130d2ae185c74e01a64f245e27b7
Perucia <peruciaaa@gmail.com>
2024-08-15 14:24:12 +0800

    fix penacony same ele buff

--- Planars/Penacony.py
+++ Planars/Penacony.py
@@ -5,14 +5,13 @@ from Delay import *
 class Penacony(Planar):
     name = "Penacony, Land of the Dreams"
     
-    def __init__(self, wearerRole: str, dpsEle: str, selfEle: str):
+    def __init__(self, wearerRole: str, sameEleTeammates: list[str]):
         super().__init__(wearerRole)
-        self.dpsEle = dpsEle
-        self.selfEle = selfEle
+        self.sameEle = sameEleTeammates
         
     def equip(self):
         buffList, debuffList, advList, delayList = super().equip()
         buffList.append(Buff("PenaconyERR", "ERR%", 0.05, self.wearerRole, ["ALL"], 1, 1, "SELF", "PERM"))
-        if self.dpsEle == self.selfEle:
-            buffList.append(Buff("PenaconyDMG", "DMG%", 0.1, "DPS", ["ALL"], 1, 1, "DPS", "PERM"))
+        for role in self.sameEle:
+            buffList.append(Buff("PenaconyDMG", "DMG%", 0.1, role, ["ALL"], 1, 1, role, "PERM"))
         return buffList, debuffList, advList, delayList
